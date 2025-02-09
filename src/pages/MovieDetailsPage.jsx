68dd34c8fd164a0e730e160f1fedbc045e531cad
andreapanico12 <andreapanico12@gmail.com>
2025-02-09 16:12:04 +0100

    feat: add base layout of MovieDetailsPage

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useGlobalContext } from "../context/GlobalContext";
 import { useEffect } from "react";
+import Rating from "../components/Rating";
 
 const MovieDetailsPage = () => {
 
@@ -10,9 +11,27 @@ const MovieDetailsPage = () => {
   useEffect(() =>fetchSingleMovie(id), [])
 
   return (
-    <div>
-      Movie {movie.title} Details and Reviews page
+    <div className="container mt-5">
+    <div className="row align-items-center justify-content-center">
+      <div className="col-md-4">
+        <h1 className="text-warning">{movie.title}</h1>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item"><strong>Director:</strong> {movie.director}</li>
+          <li className="list-group-item"><strong>Genre:</strong> {movie.genre}</li>
+          <li className="list-group-item"><strong>Abstract:</strong> {movie.abstract}</li>
+          <li className="list-group-item"><strong>Rating:</strong> <Rating vote={movie.rating} /></li>
+        </ul>
+      </div>
+
+      <div className="col-md-4 text-center">
+        <img
+          src={movie.image}
+          alt={movie.title}
+          className="img-fluid rounded movie-poster"
+        />
+      </div>
     </div>
+  </div>
   )
 }
 
